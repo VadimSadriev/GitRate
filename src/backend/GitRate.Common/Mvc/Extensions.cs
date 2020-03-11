44ab334404dab7ccb712ca000d29743cbea1dfb4
Vadim Sadriev <vadimsadrievtw@gmail.com>
2020-03-12 01:10:50 +0500

    add common project

---
+++ src/backend/GitRate.Common/Mvc/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json.Serialization;
+
+namespace GitRate.Common.Mvc
+{
+    public static class Extensions
+    {
+        public static IMvcBuilder AddCustomMvc(this IServiceCollection services)
+        {
+            return services.AddControllers()
+                 .AddJsonOptions(options =>
+                 {
+                     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                 });
+        }
+    }
+}
--- src/backend/GitRate.Web.Auth/Startup.cs
+++ src/backend/GitRate.Web.Auth/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GitRate.Common.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -24,7 +25,7 @@ namespace GitRate.Web.Auth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddCustomMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
