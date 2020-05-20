817c8a2042765b765557cffc5479615906fc68a7
Садриев Вадим <SadrievVF@it2g.ru>
2020-05-20 17:56:41 +0500

    Add signup command, empty usermanager service, empty signup handler

---
+++ src/backend/Auth.Application/Handlers/SignUpUserCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Auth.Application.Commands;
+using Auth.Persistence;
+using GitRate.Common.Identity.Types;
+using MediatR;
+
+namespace Auth.Application.Handlers
+{
+    public class SignUpUserCommandHandler : IRequestHandler<SignUpUserCommand>
+    {
+        private AuthContext _context;
+        private IUserManager _userManager;
+        
+        public SignUpUserCommandHandler(AuthContext context, IUserManager userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        
+        public Task<Unit> Handle(SignUpUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- src/backend/GitRate.Common/Identity/Types/IUserManager.cs
+++ src/backend/GitRate.Common/Identity/Types/IUserManager.cs
@@ -1,7 +1,9 @@
-﻿namespace GitRate.Common.Identity.Types
+﻿using System.Threading.Tasks;
+
+namespace GitRate.Common.Identity.Types
 {
     public interface IUserManager
     {
-        
+        public Task<string> AddAsync(string userName, string email, string password);
     }
 }
