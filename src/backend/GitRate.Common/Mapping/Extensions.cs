88b4bbbb03f7c63b7cc0cc32444e9701537b7af0
Садриев Вадим <SadrievVF@it2g.ru>
2020-05-22 18:49:47 +0500

    Add Automapper Di extension

---
+++ src/backend/GitRate.Common/Mapping/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using AutoMapper;
+using GitRate.Common.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GitRate.Common.Mapping
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMappingProfiles(this IServiceCollection services, Assembly rootAssembly)
+        {
+            var assemblies = rootAssembly
+                .GetAllReferencedAssemblies(x => x.FullName.Contains("Auth") || x.FullName.Contains("GitRate"));
+            services.AddAutoMapper(assemblies);
+            
+            return services;
+        }
+    }
+}
