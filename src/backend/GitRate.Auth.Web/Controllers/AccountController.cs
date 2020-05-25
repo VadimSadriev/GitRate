48e5501ae84f38e6ba2018671cc8ced8d7cad178
Садриев Вадим <SadrievVF@it2g.ru>
2020-05-25 19:38:53 +0500

    signup api

---
+++ src/backend/GitRate.Auth.Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Auth.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitRate.Auth.Web.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Route("signup")]
+        public async Task<IActionResult> SignUp([FromBody] SignUpUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+    }
+}
