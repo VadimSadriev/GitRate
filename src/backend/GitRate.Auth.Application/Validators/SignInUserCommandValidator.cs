38804907a21760b19a82ea900763c78434671498
Садриев Вадим <SadrievVF@it2g.ru>
2020-05-28 15:32:48 +0500

    Add SignInCommand validator

---
+++ src/backend/GitRate.Auth.Application/Validators/SignInUserCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using Auth.Application.Commands;
+using FluentValidation;
+
+namespace Auth.Application.Validators
+{
+    public class SignInUserCommandValidator : AbstractValidator<SignInUserCommand>
+    {
+        public SignInUserCommandValidator()
+        {
+            RuleFor(x => x.UserNameOrEmail)
+                .NotEmpty()
+                .WithMessage("UserName or Email cannot be empty")
+                .NotNull()
+                .WithMessage("Please, provide UserName or Email");
+            
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password cannot be empty.")
+                .NotNull()
+                .WithMessage("Please provide Password");
+        }
+    }
+}
