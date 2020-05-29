671aff432c914a55be9ebe4ec18a62ae981ebb18
Садриев Вадим <SadrievVF@it2g.ru>
2020-05-29 20:04:52 +0500

    Add RefreshJwtCommand Validator

---
+++ src/backend/GitRate.Auth.Application/Commands/RefreshJwtCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth.Application.Commands
+{
+    public class RefreshJwtCommand
+    {
+        public string Jwt { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ src/backend/GitRate.Auth.Application/Validators/RefreshJwtCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using Auth.Application.Commands;
+using FluentValidation;
+
+namespace Auth.Application.Validators
+{
+    public class RefreshJwtCommandValidator : AbstractValidator<RefreshJwtCommand>
+    {
+        public RefreshJwtCommandValidator()
+        {
+            RuleFor(x => x.Jwt)
+                .NotNull()
+                .WithMessage("Please, provide jwt.")
+                .NotEmpty()
+                .WithMessage("Jwt cannot be empty.");
+
+            RuleFor(x => x.RefreshToken)
+                .NotNull()
+                .WithMessage("Please, provide refresh token.")
+                .NotEmpty()
+                .WithMessage("Refresh token cannot be empty.");
+        }
+    }
+}
