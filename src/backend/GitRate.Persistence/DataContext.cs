9d3a20871d9a9cc4b034f58b8800653d96687eb7
Садриев Вадим <SadrievVF@it2g.ru>
2020-06-02 16:32:12 +0500

    Add DataContext, context factory. add packages.

---
+++ src/backend/GitRate.Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GitRate.Persistence
+{
+    /// <summary>
+    /// Main GitRate data context
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Main GitRate data context
+        /// </summary>
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ src/backend/GitRate.Persistence/DataContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using GitRate.Common.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace GitRate.Persistence
+{
+    public class DataContextFactory : DesignTimeDbContextFactoryBase<DataContext> 
+    {
+        protected override string BasePath => $"{Directory.GetCurrentDirectory()}/../GitRate.Web.Api";
+        
+        protected override DataContext CreateContext(DbContextOptions<DataContext> options)
+        {
+            return new DataContext(options);
+        }
+    }
+}
