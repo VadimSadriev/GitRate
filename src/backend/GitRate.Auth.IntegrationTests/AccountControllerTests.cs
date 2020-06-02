f1e5752ac8319a5270057e0596e2c0b9c5287b06
Садриев Вадим <SadrievVF@it2g.ru>
2020-06-02 14:45:32 +0500

    More tests for account controller

--- src/backend/GitRate.Auth.IntegrationTests/AccountControllerTests.cs
+++ src/backend/GitRate.Auth.IntegrationTests/AccountControllerTests.cs
@@ -42,6 +42,34 @@ namespace GitRate.Auth.IntegrationTests
             responseModel.Jwt.Should().NotBeNullOrEmpty();
             responseModel.RefreshToken.Should().NotBeNullOrEmpty();
         }
+
+        [Theory]
+        [InlineData("", "Alice@gmail.com", "Alice12345")]
+        [InlineData("Alice", "", "Alice12345")]
+        [InlineData("Alice", "Alice@gmail.com", "")]
+        public async Task SignUp_Should_Fail_With_Empty_Data(string userName, string email, string password)
+        {
+            // arrange 
+            var signUpUserCommand = new SignUpUserCommand
+            {
+                UserName = userName,
+                Email = email,
+                Password = password
+            };
+            
+            // act
+            var response = await TestClient.PostAsJsonAsync(_signUpUri, signUpUserCommand);
+
+            // assert
+            response.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+            response.Content.Should().NotBeNull();
+            response.Content.Headers.ContentType.MediaType.Should().Be(MediaTypeNames.Application.Json);
+
+            var responseModel = await response.Content.ReadAsAsync<ExceptionContract>();
+
+            responseModel.Should().NotBeNull();
+            responseModel.Errors.Should().NotBeNullOrEmpty();
+        }
         
         [Fact]
         public async Task Should_SignIn()
@@ -69,6 +97,7 @@ namespace GitRate.Auth.IntegrationTests
             // act
             var signInResponse = await TestClient.PostAsJsonAsync(_signInUri, signInUserCommand);
 
+            // assert
             signInResponse.Should().NotBeNull();
             signInResponse.StatusCode.Should().Be(StatusCodes.Status200OK);
             signInResponse.Content.Should().NotBeNull();
@@ -79,6 +108,47 @@ namespace GitRate.Auth.IntegrationTests
             signInResponseModel.Should().NotBeNull();
             signInResponseModel.Jwt.Should().NotBeNullOrEmpty();
             signInResponseModel.RefreshToken.Should().NotBeNullOrEmpty();
+        }
+
+        [Theory]
+        [InlineData("", "Alice12345")]
+        [InlineData("Alice", "")]
+        public async Task SignIn_Should_Fail_With_Empty_Data(string userNameOrEmail, string password)
+        {
+            // arrange
+            var signUpUserCommand = new SignUpUserCommand
+            {
+                UserName = "Alice",
+                Email = "Alice@gmail.com",
+                Password = "Alice12345"
+            };
+            
+            var signUpResponse = await TestClient.PostAsJsonAsync(_signUpUri, signUpUserCommand);
+            
+            signUpResponse.StatusCode.Should().Be(StatusCodes.Status200OK);
+            signUpResponse.Content.Should().NotBeNull();
+            signUpResponse.Content.Headers.ContentType.MediaType.Should().Be(MediaTypeNames.Application.Json);
+
+            var signInUserCommand = new SignInUserCommand
+            {
+                UserNameOrEmail = userNameOrEmail,
+                Password = password
+            };
+
+            // act
+            var signInResponse = await TestClient.PostAsJsonAsync(_signInUri, signInUserCommand);
+
+            // assert
+            signInResponse.Should().NotBeNull();
+            signInResponse.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+            signInResponse.Content.Should().NotBeNull();
+            signInResponse.Content.Headers.ContentType.MediaType.Should().Be(MediaTypeNames.Application.Json);
+
+            var signInResponseModel = await signInResponse.Content.ReadAsAsync<ExceptionContract>();
+
+            signInResponseModel.Should().NotBeNull();
+            signInResponseModel.Should().NotBeNull();
+            signInResponseModel.Errors.Should().NotBeNullOrEmpty();
         }
     }
 }
