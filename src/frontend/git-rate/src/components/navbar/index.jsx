4983585f000d352ba1c9c3ab149d51c359043bce
Садриев Вадим <SadrievVF@it2g.ru>
2020-06-03 11:13:12 +0500

    Add material ui package. rename extensions

---
+++ src/frontend/git-rate/src/components/navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function NavBar(props) {
+
+    return (
+        <React.Fragment>
+            NavBar
+        </React.Fragment>
+    )
+}
